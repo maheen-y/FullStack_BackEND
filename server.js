45dfcff93f64151d1407563cd3e90ea0485bce8c
maheen-y <maheenyassar@gmail.com>
2025-11-27 14:37:02 +0000

    Port fixed

--- server.js
+++ server.js
@@ -116,4 +116,5 @@ app.put("/lessons/:id", async (req, res) => {
 });
 
 // App listens on port 3000
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
